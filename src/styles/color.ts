322d35c99aefa565d9953f905d97a57e80114466
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-03-28 23:26:34 +0900

    color 컴포넌트 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,11 @@
+export const color = {
+  gray: {
+    gray1: '#EAEAEA',
+    gray2: '#CECECE',
+    gray3: '#898989',
+    white: '#FFFFFF',
+  },
+  color: {
+    lightPurple: '#E1D2FF',
+  },
+};
