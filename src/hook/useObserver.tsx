24581019257b56a03463c729abbe274c80f4f6de
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-04-05 18:05:04 +0900

    useObserver 수정

--- src/hook/useObserver.tsx
+++ src/hook/useObserver.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef } from 'react';
 
 export const useObserver = (navName: string, setNavName: React.Dispatch<React.SetStateAction<string>>) => {
-  const options = { threshold: 0.5 };
+  const options = { threshold: 0.7 };
   const refElement = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries, observer) => {
-      console.log(entries[0], navName);
       if (entries[0].isIntersecting) {
         setNavName(navName);
       }
